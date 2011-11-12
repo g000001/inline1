d986a243e24b9b99922bebc17ba93fd44e4aa949
g000001 <chiba.masaomi@gmail.com>
2011-11-13 06:34:54 +0900

    sbclもcompiler-letはインポートするようにした

--- package.lisp
+++ package.lisp
@@ -8,8 +8,9 @@
   (:export :defun-inline-self))
 
 (defpackage :root.function.inline1-internal
-  (:use :inline1 :cl :fiveam #+sbcl :sb-cltl2)
+  (:use :inline1 :cl :fiveam)
   (:import-from #+allegro :excl
-                #+(or cmu :scl clisp) :ext
+                #+(or cmu scl clisp) :ext
                 #+ccl :ccl
+                #+sbcl :sb-cltl2
                 :compiler-let))
