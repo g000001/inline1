d3315ba6edff5eb3e60b372ff956392017b9b016
g000001 <chiba.masaomi@gmail.com>
2011-11-12 15:48:57 +0900

    無駄に、Clozure CL、Allegro CL、Scieneer CL、CLISP にも対応してみた。 あまり意味がないテストがなければ、GCLでも動く。

--- package.lisp
+++ package.lisp
@@ -8,5 +8,8 @@
   (:export :defun-inline-self))
 
 (defpackage :root.function.inline1-internal
-  (:use :inline1 :cl :fiveam #+sbcl :sb-cltl2))
-
+  (:use :inline1 :cl :fiveam #+sbcl :sb-cltl2)
+  (:import-from #+allegro :excl
+                #+(or cmu :scl clisp) :ext
+                #+ccl :ccl
+                :compiler-let))
